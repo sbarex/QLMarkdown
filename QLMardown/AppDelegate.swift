23859015ff9ddd8d6e3e93d18d5a4aaff24c529d
sbarex <sbarex@gmail.com>
2020-12-09 23:32:43 +0100

    Initial Commit

---
+++ QLMardown/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QLMardown
+//
+//  Created by Simone Baldissini on 09/12/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QLMardown/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QLMardown
+//
+//  Created by Simone Baldissini on 09/12/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
